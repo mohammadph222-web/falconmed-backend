2e6104f76fe2ea8034525223f3c7a6b042a57e82
mohammadph222-web <mohammad.ph.222@gmail.com>
2026-09-18 19:20:27 +0400

    "fix: Add localhost:5174 to CORS origins"

--- src/server.js
+++ src/server.js
@@ -16,8 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://falconmed.app', 'https://...'],
-  credentials: true,
+origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'https://falconmed.app', 'https://falconmed.vercel.app', '*'],  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
